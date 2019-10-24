2b98460d6e3a8d154384f941c5351bcdc5d6074e
Peter Kay <contact@kaypeter.com>
2019-10-24 12:04:06 -0400

    move write error to the class level

--- src/remove-sfxmember.ps1
+++ src/remove-sfxmember.ps1
@@ -16,13 +16,7 @@ function Remove-SFxMember {
         if ($PSBoundParameters.ContainsKey('ApiToken')) {
             $request.SetToken($ApiToken) | Out-Null
         }
-        try {
             Write-Information $request.Uri
             $request.Invoke()
-        } catch {
-            Write-Error "StatusCode:" $_.Exception.Response.StatusCode.value__
-            Write-Error "StatusDescription:" $_.Exception.Response.StatusDescription
-        }
     }
-
 }
