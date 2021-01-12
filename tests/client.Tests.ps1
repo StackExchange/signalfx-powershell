bcd44e62ae19968a352383d568c3c790d1d7ba74
Christian Pearce <cpearce@stackoverflow.com>
2021-01-12 10:51:00 -0500

    Eliminate order_by in chaining methods test

--- tests/client.Tests.ps1
+++ tests/client.Tests.ps1
@@ -121,7 +121,7 @@ Describe "client" {
 
         It 'Should chain methods' {
             $chain = [SFxQueryMetricTimeSeries]::new('test_query').Offset(1).Limit(1)
-            $chain.Uri | Should -Be 'https://api.us1.signalfx.com/v2/metrictimeseries?query=test_query&orderby=test_key&offset=1&limit=1'
+            $chain.Uri | Should -Be 'https://api.us1.signalfx.com/v2/metrictimeseries?query=test_query&offset=1&limit=1'
         }
     }
 
